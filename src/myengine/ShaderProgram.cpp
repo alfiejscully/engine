dd722aae7b4a4d2538dd61a3a2c75d7df28bad07
alfie scully <alfiejscully@blueyonder.co.uk>
2018-11-17 05:31:07 +0000

    ShaderProgram class added and vert and frag shaders files made

---
+++ src/myengine/ShaderProgram.cpp
@@ -0,0 +1,11 @@
+#include "ShaderProgram.h"
+
+ShaderProgram::ShaderProgram(std::string _vertex, std::string _fragment)
+{
+
+
+
+
+
+
+}
---
+++ src/myengine/ShaderProgram.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <glm/glm.hpp>
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <GL/glew.h>
+
+class ShaderProgram
+{
+private:
+
+
+public:
+	ShaderProgram(std::string _vertex, std::string _fragment);
+
+};
