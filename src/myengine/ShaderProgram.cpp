a722c9a40e904a3f2eefa7ced8c4bf6cd6745116
alfie scully <alfiejscully@blueyonder.co.uk>
2018-11-18 01:32:10 +0000

    further work on ShaderProgram class

--- src/myengine/ShaderProgram.cpp
+++ src/myengine/ShaderProgram.cpp
@@ -23,7 +23,7 @@ ShaderProgram::ShaderProgram(std::string _vertex, std::string _fragment)
 
 	m_file.open(_fragment.c_str()); //opens fragment file 
 
-	if (!m_file.is_open()) //checks if fragment file is open 
+	if (!m_file.is_open()) //checks if fragment file is not opening  
 	{
 		throw std::exception();
 	}
@@ -38,8 +38,46 @@ ShaderProgram::ShaderProgram(std::string _vertex, std::string _fragment)
 		m_fragmentSrc += m_line + "\n";
 	}
 
+	m_file.close(); //closes the fragment file
 
+	// Create and compile the vertex shader
+	m_vertexShader = m_vertexSrc.c_str();
+	m_vertexShaderID = glCreateShader(GL_VERTEX_SHADER); 
+	glShaderSource(m_vertexShaderID, 1, &m_vertexShader, NULL);
+	glCompileShader(m_vertexShaderID);
+	glGetShaderiv(m_vertexShaderID, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		throw std::exception();
+	}
+	else
+	{
+		std::cout << "Vertex Working\n";
+	}
+
+	// Create and compile the fragment shader
+	m_fragmentShader = m_fragmentSrc.c_str();
+	m_fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(m_fragmentShaderID, 1, &m_fragmentShader, NULL);
+	glCompileShader(m_fragmentShaderID);
+	glGetShaderiv(m_fragmentShaderID, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		throw std::exception();
+	}
+	else
+	{
+		std::cout << "Fragment Working\n";
+	}
+
+	m_shaderProgramID = glCreateProgram();
+	glAttachShader(m_shaderProgramID, m_vertexShaderID);
+	glAttachShader(m_shaderProgramID, m_fragmentShaderID);
 }
 
 ShaderProgram::~ShaderProgram()
-{}
+{
+
+}
--- src/myengine/ShaderProgram.h
+++ src/myengine/ShaderProgram.h
@@ -1,8 +1,9 @@
 #include <string>
-#include <glm/glm.hpp>
 #include <fstream>
 #include <iostream>
 #include <exception>
+
+#include <glm/glm.hpp>
 #include <GL/glew.h>
 
 class ShaderProgram
@@ -12,6 +13,15 @@ private:
 	std::string m_vertexSrc;
 	std::string m_fragmentSrc;
 	std::string m_line;
+
+	const GLchar *m_vertexShader;
+	const GLchar *m_fragmentShader;
+
+	GLuint m_vertexShaderID;
+	GLuint m_fragmentShaderID;
+	GLuint m_shaderProgramID;
+
+	GLint success = 0;
 
 public:
 	ShaderProgram(std::string _vertex, std::string _fragment);
